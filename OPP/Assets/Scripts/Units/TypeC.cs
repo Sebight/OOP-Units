7c1ea478585f2311ffb0d29ae57d1fe4c3a9c607
sebi210dev <59232422+sebi210dev@users.noreply.github.com>
2021-05-11 19:44:28 +0200

    Add first sample callback delegates

--- OPP/Assets/Scripts/Units/TypeC.cs
+++ OPP/Assets/Scripts/Units/TypeC.cs
@@ -12,6 +12,12 @@ public class TypeC : Unit
         lineRenderer = GetComponent<LineRenderer>();
     }
 
+
+    public void RegisterOnArrive(ArrivedDelegate callback)
+    {
+        arrivedDelegate = callback;
+    }
+
     public override void Action(RaycastHit hit)
     {
         Unit unitComponent = hit.transform.gameObject.GetComponent<Unit>();
@@ -20,26 +26,42 @@ public class TypeC : Unit
             if (!coroutineStarted)
             {
                 base.Action(hit);
-                StartCoroutine(Shoot(unitComponent));
+                //StartCoroutine(Shoot(unitComponent));
+
+                RegisterOnArrive(() => StartCoroutine(Shoot(unitComponent)));
+                
+
+                //TODO: create dynamic on arrive system
+                //RegisterOnArrive(Shoot);
+                //Move()
+
+                // delegate /w event
+
+                // Action / Func
+
+                /*
+                  System.Action<Vector3, Vector3> met;
+
+                  met = ShowLine;
+
+                  met(Vector3.zero, Vector3.one);
+                  */
             }
         }
     }
 
-    public IEnumerator Shoot(Unit otherUnit)
+    IEnumerator Shoot(Unit otherUnit)
     {
-        if (!coroutineStarted)
-        {
-            coroutineStarted = true;
-            while (!(Vector3.Distance(agent.destination, agent.transform.position) <= agent.stoppingDistance)) yield return null;
-            yield return new WaitForSeconds(0.5f);
-            lineRenderer.SetPosition(0, gameObject.transform.position + new Vector3(0, 1, 0));
-            lineRenderer.SetPosition(1, otherUnit.gameObject.transform.position + new Vector3(0, 1, 0));
-            lineRenderer.enabled = true;
-            otherUnit.gameObject.SetActive(false);
-            yield return new WaitForSeconds(1);
-            lineRenderer.enabled = false;
-            coroutineStarted = false;
-        }
+        Debug.Log("YEEHAW");
+        yield return new WaitForSeconds(1);
+        lineRenderer.SetPosition(0, gameObject.transform.position + new Vector3(0, 1, 0));
+        lineRenderer.SetPosition(1, otherUnit.gameObject.transform.position + new Vector3(0, 1, 0));
+        lineRenderer.enabled = true;
+        otherUnit.gameObject.SetActive(false);
+        yield return new WaitForSeconds(1);
+        lineRenderer.enabled = false;
+        sentDelegate = false;
+        arrivedDelegate = null;
     }
 
     public override void OnSelect()
--- OPP/Assets/Scripts/Units/Unit.cs
+++ OPP/Assets/Scripts/Units/Unit.cs
@@ -8,6 +8,12 @@ public class Unit : MonoBehaviour
     /*[SerializeField]*/ public NavMeshAgent agent;
     public Outline outline;
 
+    public bool sentDelegate = false;
+    public event System.Action<Unit> arrivedAction;
+
+    public delegate void ArrivedDelegate();
+    public ArrivedDelegate arrivedDelegate;
+
     public virtual void Move(Vector3 position)
     {
         agent.SetDestination(position);
@@ -36,5 +42,20 @@ public class Unit : MonoBehaviour
     public void HideOutline()
     {
         outline.enabled = false;
+    }
+
+
+    public void Update()
+    {
+        bool distanceSmaller = (Vector3.Distance(agent.destination, agent.transform.position) <= agent.stoppingDistance);
+        if (distanceSmaller && !sentDelegate)
+        {
+            if (arrivedDelegate != null)
+            {
+                Debug.Log("SOMETHING I SSUBCRISD");
+                arrivedDelegate?.Invoke();
+                sentDelegate = true;
+            }
+        }
     }
 }
